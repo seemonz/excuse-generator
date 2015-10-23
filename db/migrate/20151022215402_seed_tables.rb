06afd9c10bdac228ca4cf8efe703efbfef5d9c34
William Fan <fan.wenhao@gmail.com>
2015-10-23 23:04:30 +0000

    Saving function completed

--- db/migrate/20151022215402_seed_tables.rb
+++ db/migrate/20151022215402_seed_tables.rb
@@ -12,3 +12,19 @@ class SeedTables < ActiveRecord::Migration
     Final.create(phrase: "I punched it.")
   end
 end
+
+
+# Middle.create(phrase: "I saw a robbery happening nearby")
+# Middle.create(phrase: "there was a car accident near me")
+# Middle.create(phrase: "a sparkly fairy appeared next to me out of nowhere")
+# Middle.create(phrase: "Gandalf was walking towards me with his staff raised high")
+# Middle.create(phrase: "Ninja Turtles called and said they needed my help")
+# Middle.create(phrase: "my cousin's neighbour's grandparent's nephew's eldest daughter's pet turtle died")
+# Middle.create(phrase: "there was a sketchy figure poking around near Lighthouse Labs")
+# Middle.create(phrase: "an alien ship descended near me")
+# Middle.create(phrase: "it was pouring rain near my house")
+# Middle.create(phrase: "...errr...yaa...something something...")
+# Final.create(phrase: "I did a German suplex on it.")
+# Final.create(phrase: "I turned around to continue sleeping on my comfy bed")
+# Final.create(phrase: "I opened the wardrobe and stepped into Narnia.")
+# Final.create(phrase: "I slept for the entire day.")
---
+++ db/migrate/20151023133402_add_upvote_count.rb
@@ -0,0 +1,8 @@
+class AddUpvoteCount < ActiveRecord::Migration
+
+  def change
+    add_column :excuses, :upvote_count, integer
+    end
+  end
+
+end
