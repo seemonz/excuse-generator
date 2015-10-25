76d0722694c917e023d3767063968eaf91c0cd79
vagrant <vagrant@lighthouse-labs>
2015-10-25 23:49:12 +0000

    refactored routes, completed upvote

--- app/actions.rb
+++ app/actions.rb
@@ -1,15 +1,11 @@
 helpers do
 
-  def save_excuse
-    current = Excuse.where(:beginnings_id => @beginning.id, :middles_id => @middle.id, :finals_id => @final.id).first_or_create
-    current
-    #binding.pry
+  def category_valid?
+    params[:category] == 'late' || params[:category] == 'absent' || params[:category] == 'dont_want_to' || params[:category] == 'dont_have_it'
+  end
 
-    # if excuse_repeat?
-    #   @excuse = Excuse.where("beginnings_id = ? AND middles_id = ? AND finals_id = ?", @beginning.id, @middle.id, @final.id)
-    # else
-    #   @excuse.save
-    # end
+  def save_excuse
+    Excuse.where(:beginnings_id => @beginning.id, :middles_id => @middle.id, :finals_id => @final.id).first_or_create
   end
 
   # def excuse_repeat?
@@ -20,10 +16,6 @@ helpers do
     Excuse.limit(5).order('upvote_count desc') # this works correctly
   end
 
-  # def current_excuse 
-  #   @current_excuse = save_excuse
-  # end
-
 end
 
 
@@ -33,59 +25,66 @@ get '/' do
   erb :excuse_gen
 end
 
-# button click routes
-get '/late' do 
-  #WORRY ABOUT /NIL OR /GARBARGEGEGE
-  # binding.pry
-  @beginning = Beginning.get_phrase('late')
-  @middle = Middle.get_phrase
-  @final = Final.get_phrase
-  @current = save_excuse
-  @top5_excuses = top_excuses
-  erb :excuse_gen
-end
-
-get '/absent' do 
-  #WORRY ABOUT /NIL OR /GARBARGEGEGE
-  # binding.pry
-  @beginning = Beginning.get_phrase('absent')
-  @middle = Middle.get_phrase
-  @final = Final.get_phrase
-  save_excuse
-  @top5_excuses = top_excuses
-  erb :excuse_gen
-end
-
-get '/dont_want_to' do 
-  #WORRY ABOUT /NIL OR /GARBARGEGEGE
-  # binding.pry
-  @beginning = Beginning.get_phrase('dont_want_to')
-  @middle = Middle.get_phrase
-  @final = Final.get_phrase
-  save_excuse
-  @top5_excuses = top_excuses
-  erb :excuse_gen
-end
-
-get '/dont_have_it' do 
-  #WORRY ABOUT /NIL OR /GARBARGEGEGE
-  # binding.pry
-  @beginning = Beginning.get_phrase('dont_have_it')
-  @middle = Middle.get_phrase
-  @final = Final.get_phrase
-  save_excuse
-  @top5_excuses = top_excuses
-  erb :excuse_gen
-end
-
 post '/upvote' do
   # not sure how to select the current excuse displayed. we need to grab that excuse call the increment_upvote method and save it.
   @top5_excuses = top_excuses
-  vote_excuse = @current
-  binding.pry
-  vote_excuse.increment_upvote
-  vote_excuse.save
+  @current_excuse = Excuse.find(params['current_excuse'])
+  @current_excuse.increment_upvote
+  @current_excuse.save
   @upvoted = true
+  @beginning = @current_excuse.beginning
+  @middle = @current_excuse.middle
+  @final = @current_excuse.final
   erb :excuse_gen
 end
+
+# button click routes
+get '/:category' do 
+  if category_valid?
+    @beginning = Beginning.get_phrase(params[:category])
+    @middle = Middle.get_phrase
+    @final = Final.get_phrase
+    @current = save_excuse
+    @top5_excuses = top_excuses
+    erb :excuse_gen
+  else
+    @top5_excuses = top_excuses
+    redirect '/'
+  end
+end
+
+# get '/absent' do 
+#   #WORRY ABOUT /NIL OR /GARBARGEGEGE
+#   # binding.pry
+#   @beginning = Beginning.get_phrase('absent')
+#   @middle = Middle.get_phrase
+#   @final = Final.get_phrase
+#   save_excuse
+#   @top5_excuses = top_excuses
+#   erb :excuse_gen
+# end
+
+# get '/dont_want_to' do 
+#   #WORRY ABOUT /NIL OR /GARBARGEGEGE
+#   # binding.pry
+#   @beginning = Beginning.get_phrase('dont_want_to')
+#   @middle = Middle.get_phrase
+#   @final = Final.get_phrase
+#   save_excuse
+#   @top5_excuses = top_excuses
+#   erb :excuse_gen
+# end
+
+# get '/dont_have_it' do 
+#   #WORRY ABOUT /NIL OR /GARBARGEGEGE
+#   # binding.pry
+#   @beginning = Beginning.get_phrase('dont_have_it')
+#   @middle = Middle.get_phrase
+#   @final = Final.get_phrase
+#   save_excuse
+#   @top5_excuses = top_excuses
+#   erb :excuse_gen
+# end
+
+
 
